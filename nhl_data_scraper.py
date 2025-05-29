a4f07b3eb4fed3c9cb12742c29888789cbc7973d
muw33n <muween@gmail.com>
2025-05-29 10:03:51 +0200

    NHL data scraper - upraveno kódování logovacího souboru na UTF8

--- nhl_data_scraper.py
+++ nhl_data_scraper.py
@@ -19,7 +19,7 @@ logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s - %(levelname)s - %(message)s',
     handlers=[
-        logging.FileHandler('logs/scraper.log'),
+        logging.FileHandler('logs/scraper.log', encoding='utf-8'),
         logging.StreamHandler()
     ]
 )
